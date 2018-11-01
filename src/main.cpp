58ad40d1bdb80e1e5721424b6d6ca1caad3d07bf
Oskar Weigl <oskar.weigl@gmail.com>
2018-10-31 22:21:24 -0700

    add read from file option to main test

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,10 @@
 #include "ESKF.h"
 #include <iostream>
 #include <chrono>
+#include <fstream>
+#include <cstdio>
 
-#define GRAVITY 	9.812  // London g value.
+#define GRAVITY 9.812f  // London g value.
 #define SQ(x) (x*x)
 #define I_3 (Eigen::Matrix3f::Identity())
 #define I_dx (Eigen::Matrix<float, dSTATE_SIZE, dSTATE_SIZE>::Identity())
@@ -29,8 +31,8 @@ int main(int argc, char** argv) {
     float sigma_init_accel_bias = 100*sigma_accel_drift; // [m/s^2]
     float sigma_init_gyro_bias = 100*sigma_gyro_drift; // [rad/s]
 
-    float sigma_mocap_pos = 0.001; // [m]
-    float sigma_mocap_rot = 0.01; // [rad]
+    float sigma_mocap_pos = 0.003; // [m]
+    float sigma_mocap_rot = 0.03; // [rad]
 
     ESKF eskf(
             Vector3f(0, 0, -GRAVITY), // Acceleration due to gravity in global frame
@@ -53,42 +55,100 @@ int main(int argc, char** argv) {
             SQ(sigma_accel_drift),
             SQ(sigma_gyro_drift));
 
-    //timing some key things just for some insight. Accell is 1000hz and mocap is 100hz.
-    float sim_data_duration = 1000.0f; //seconds
-    auto start = std::chrono::system_clock::now();
-    for (int ms = 0; ms < 1000*sim_data_duration; ms++) {
+    if (argc >= 2) {
+        // Run from file
+        std::ifstream infile(argv[1]);
+        std::ofstream outfile("out.txt");
+        outfile.precision(20);
+        std::string line;
+        std::string IMU_Prefix("IMU");
+        std::string MOCAP_Prefix("Mocap");
+        float accel_units = 9.81;
+        float gyro_units = 360.0f / (2 * M_PI);
+        IOFormat CsvStyle(StreamPrecision, DontAlignCols, ", ", ", ", "", "", "", "");
+        while (std::getline(infile, line)) {
+            double t = 0;
+            if(!line.compare(0, IMU_Prefix.length(), IMU_Prefix)) {
+                // Process IMU
+                Vector3f accel;
+                Vector3f gyro;
+                unsigned int sec, nsec;
+                sscanf(line.c_str(),
+                        "IMU,%f,%f,%f,%f,%f,%f,%u,%u",
+                        &accel(0), &accel(1), &accel(2),
+                        &gyro(0), &gyro(1), &gyro(2),
+                        &sec, &nsec);
+                t = (double)sec + (double)nsec * 1e-9;
+                accel.array() /= accel_units;
+                gyro.array() /= gyro_units;
 
-        // Simulated static true pos/orientation
-        Vector3f pos_true = Vector3f(0, 0, 0);
-        Quaternionf q_true = Quaternionf(AngleAxisf(0.0f, Vector3f(1, 0, 0)));
-        // Quaternionf q_true = Quaternionf(AngleAxisf(0.002f*ms, Vector3f(1, 0, 0)));
-        Matrix3f R_true = q_true.toRotationMatrix();
+                eskf.predictIMU(accel, gyro, 0.001f);
+            } else if(!line.compare(0, MOCAP_Prefix.length(), MOCAP_Prefix)) {
+                // Process mocap
+                Vector3f pos_meas;
+                Quaternionf q_meas;
+                unsigned int sec, nsec;
+                unsigned int rec_sec, rec_nsec;
+                sscanf(line.c_str(),
+                        "Mocap,%f,%f,%f,%f,%f,%f,%f,%u,%u,%u,%u",
+                        &pos_meas(0), &pos_meas(1), &pos_meas(2),
+                        &q_meas.x(), &q_meas.y(), &q_meas.z(), &q_meas.w(),
+                        &sec, &nsec, &rec_sec, &rec_nsec);
+                t = (double)rec_sec + (double)rec_nsec * 1e-9;
 
-        // Fake accel/gyro
-        Vector3f acc_true = R_true.transpose() * Vector3f(0, 0, GRAVITY);
-        Vector3f acc = acc_true + sigma_accel * Vector3f::Random();
-        Vector3f gyro = sigma_gyro * Vector3f::Random();
-        // Input our accel/gyro data
-        eskf.predictIMU(acc, gyro, 0.001f);
+                eskf.measurePos(pos_meas, SQ(sigma_mocap_pos)*I_3);
+                eskf.measureQuat(q_meas, SQ(sigma_mocap_rot)*I_3);
+            } else {
+                continue;
+            }
 
-        // 10 accel/gyro measurements per motion capture input.
-        if (ms % 10 == 0) {
-            // Fake mocap data
-            Vector3f pos_meas = pos_true + sigma_mocap_pos * Vector3f::Random();
-            Quaternionf q_meas = q_true;
-            // input our motion capture data
-            eskf.measurePos(pos_meas, SQ(sigma_mocap_pos)*I_3);
-            eskf.measureQuat(q_meas, SQ(sigma_mocap_rot)*I_3);
+            outfile << t << "," <<
+                    eskf.getPos().format(CsvStyle) <<
+                    eskf.getVel().format(CsvStyle) <<
+                    eskf.getQuatVector().format(CsvStyle) <<
+                    eskf.getAccelBias().format(CsvStyle) <<
+                    eskf.getGyroBias().format(CsvStyle) <<
+                    std::endl;
         }
     }
-    auto end = std::chrono::system_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << elapsed.count() << std::endl;
+    else {
+        // Run with self generated data
+        //timing some key things just for some insight. Accell is 1000hz and mocap is 100hz.
+        float sim_data_duration = 1000.0f; //seconds
+        auto start = std::chrono::system_clock::now();
+        for (int ms = 0; ms < 1000 * sim_data_duration; ms++) {
+
+            // Simulated static true pos/orientation
+            Vector3f pos_true = Vector3f(0, 0, 0);
+            Quaternionf q_true = Quaternionf(AngleAxisf(0.0f, Vector3f(1, 0, 0)));
+            // Quaternionf q_true = Quaternionf(AngleAxisf(0.002f*ms, Vector3f(1, 0, 0)));
+            Matrix3f R_true = q_true.toRotationMatrix();
+
+            // Fake accel/gyro
+            Vector3f acc_true = R_true.transpose() * Vector3f(0, 0, GRAVITY);
+            Vector3f acc = acc_true + sigma_accel * Vector3f::Random();
+            Vector3f gyro = sigma_gyro * Vector3f::Random();
+            // Input our accel/gyro data
+            eskf.predictIMU(acc, gyro, 0.001f);
+
+            // 10 accel/gyro measurements per motion capture input.
+            if (ms % 10 == 0) {
+                // Fake mocap data
+                Vector3f pos_meas = pos_true + sigma_mocap_pos * Vector3f::Random();
+                Quaternionf q_meas = q_true;
+                // input our motion capture data
+                eskf.measurePos(pos_meas, SQ(sigma_mocap_pos)*I_3);
+                eskf.measureQuat(q_meas, SQ(sigma_mocap_rot)*I_3);
+            }
+        }
+        auto end = std::chrono::system_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        std::cout << elapsed.count() << std::endl;
+    }
 
     std::cout << "Pos: " << std::endl << eskf.getPos() << std::endl;
     std::cout << "Vel: " << std::endl << eskf.getVel() << std::endl;
     std::cout << "QuatVector: " << std::endl << eskf.getQuatVector() << std::endl;
     std::cout << "AccelBias: " << std::endl << eskf.getAccelBias() << std::endl;
     std::cout << "GyroBias: " << std::endl << eskf.getGyroBias() << std::endl;
-
 }
